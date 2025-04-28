d4e6a158cb9662e063548648b32347ebd9e4ea19
Ahmed <lameda789@gmail.com>
2025-04-28 11:07:54 +0100

    build fix

--- app/provider.tsx
+++ app/provider.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react";
 import { Provider as ReduxProvider } from "react-redux";
 import { store, persistor } from "@/store";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { PersistGate } from "redux-persist/integration/react";
 import PersistentAudioPlayer from "@/store/PersistentAudioPlayer";
 
